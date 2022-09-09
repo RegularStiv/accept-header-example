3a3b3c3030e0c369ea51c73c1b312225cf4cd8e8
Stephen Rumpp (RIT Student) <sar7743@rit.edu>
2022-09-09 13:49:31 -0400

    demo done

--- src/responses.js
+++ src/responses.js
@@ -16,7 +16,10 @@ const getCats = (request, response, acceptedTypes) =>{
     name:'Roosevelt',
     age: 13
   };
-  
+  if(acceptedTypes[0] === 'text/xml'){
+    let responseXML = `<response><name>${cat.name}</name><age>${cat.age}</age></response>`;
+    return respond(request,response,responseXML,'text/xml');
+  }
   const catString = JSON.stringify(cat); 
   return respond(request,response,catString,'application/json');
 }
